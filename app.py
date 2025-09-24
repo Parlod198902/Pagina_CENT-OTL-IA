e363a9d134fb1c830996a74041ec33f0f3746700
Ivan Alexei Gamboa Bernal <ivanalexeigamboabernal@hotmail.com>
2025-09-23 23:17:26 -0600

    ACTUALIZACION DE SEGURIDAD

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USERNAME'] = 'oficialcenteotl@gmail.com'
-app.config['MAIL_PASSWORD'] = 'jdjg djfm qhqu ptvy'  # <-- Tu contraseña de aplicación
+app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD')
 app.config['MAIL_DEFAULT_SENDER'] = 'oficialcenteotl@gmail.com'
 
 mail = Mail(app)
